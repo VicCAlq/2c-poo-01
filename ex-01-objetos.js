a337749a4939cc872a179e24d4ff97af5a7787ab
Joao-Barros-NAVE <joao.guilherme@nave.org.br>
2026-04-10 11:08:48 -0300

    comita e puxa coxinha

--- ex-01-objetos.js
+++ ex-01-objetos.js
@@ -304,8 +304,13 @@ Crie um objeto chamado `aluno` com as seguintes propriedades:
 Em seguida, armazene na variável resposta1 apenas o valor da propriedade nome do objeto aluno.
 */
 // Escreva o código da solução abaixo:
-
-export const resposta1 = false
+let aluno = {
+nome: "João",
+idade: 15,
+cidade: "Recife",
+curso: "Programação"
+}
+export const resposta1 = aluno.nome
 
 /* Questão 2
 Crie um objeto chamado `produto` que represente um produto de uma loja com:
@@ -317,8 +322,14 @@ Em seguida, crie uma variável chamada `valorTotalEstoque` que calcule o valor t
 Armazene na variável resposta2 o valor total do estoque.
 */
 // Escreva o código da solução abaixo:
-
-export const resposta2 = false
+let produto = {
+nome: "Notebook",
+preco: 3500,
+quantidadeEstoque: 15,
+categoria: "Eletrônicos"
+}
+let valorTotalEstoque = produto.preco * produto.quantidadeEstoque
+export const resposta2 = valorTotalEstoque
 
 /* Questão 3
 Crie um objeto chamado `filme` com as propriedades:
@@ -331,8 +342,15 @@ Em seguida, altere o valor da propriedade disponivelStreaming para false e adici
 Armazene na variável resposta3 um array contendo todas as chaves (nomes das propriedades) do objeto filme usando Object.keys().
 */
 // Escreva o código da solução abaixo:
-
-export const resposta3 = false
+let filme = {
+titulo: "O Poderoso Chefão",
+ano: 1972,
+diretor: "Francis Ford Coppola",
+genero: "Drama",
+disponivelStreaming: false,
+nota: 9.5
+}
+export const resposta3 = Object.keys(filme)
 
 /* Questão 4
 Crie um objeto chamado `configuracoes` com as seguintes propriedades aninhadas:
@@ -358,8 +376,26 @@ Execute os seguintes passos:
 3. Armazene na variável resposta5 o resultado de obterValor()
 */
 // Escreva o código da solução abaixo:
+let contador = {
+    valor: 0,
+    incrementar: function (){
+        this.valor++
+    },
+    decrementar: function(){
+        this.valor--
+    },
+    obterValor: function(){
+         return this.valor
+    }
 
-export const resposta5 = false
+    }
+    contador.incrementar()
+    contador.incrementar()
+    contador.incrementar()
+    contador.decrementar()
+
+
+export const resposta5 = contador.obterValor()
 
 /* Questão 6
 Crie um objeto chamado `termometro` com:
@@ -410,7 +446,7 @@ Execute:
 export const resposta8 = false
 
 export const ingredientes = [ 
-  "farinha", "ovos", "leite", açú"car", "manteiga", "chocolate", 
+  "farinha", "ovos", "leite", "açúcar", "manteiga", "chocolate", 
   "fermento", "sal", "queijo", "presunto", "tomate", "cebola", 
   "alho", "óleo", "frango" 
 ]
