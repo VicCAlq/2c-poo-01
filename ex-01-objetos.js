69903e47d9c335d7e3a5adda36db7b969fdab77a
Teteuzo <matheus.thiago@nave.org.br>
2026-04-10 11:06:27 -0300

    Envio provisório

--- ex-01-objetos.js
+++ ex-01-objetos.js
@@ -305,7 +305,14 @@ Em seguida, armazene na variável resposta1 apenas o valor da propriedade nome d
 */
 // Escreva o código da solução abaixo:
 
-export const resposta1 = false
+let aluno = {
+  nome: "Severinu",
+  idade: 110,
+  cidade: "Pontezinha",
+  curso: "Computaria",
+}
+
+export const resposta1 = aluno.nome
 
 /* Questão 2
 Crie um objeto chamado `produto` que represente um produto de uma loja com:
@@ -318,7 +325,18 @@ Armazene na variável resposta2 o valor total do estoque.
 */
 // Escreva o código da solução abaixo:
 
-export const resposta2 = false
+let produto = {
+  nome: "Notebook",
+  preco: 3500,
+  quantidadeEstoque: 15,
+  categoria: "Eletrônicos",
+
+  matheusQuebraCodigu: function() {
+    produto.preco * produto.quantidadeEstoque
+  }
+} 
+
+export const resposta2 = produto.matheusQuebraCodigu
 
 /* Questão 3
 Crie um objeto chamado `filme` com as propriedades:
